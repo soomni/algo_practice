3f7d4de63d2e2accbdd2b6d13e1469fdcf4f1dee
Soomin Lee <soominlee@Soominui-MacBook-Pro.local>
2020-05-06 20:27:02 +0900

    update

---
+++ src/MinimumBribes.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class MinimumBribes {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int cnt = 0;
+        int cntTotal = 0;
+        for ( int i = 0 ; i < q.length; i++){
+            int changeVal = i - q[i];
+            cntTotal += changeVal<0? (changeVal*-1) : changeVal;
+            cnt += changeVal;
+        }
+        if( cnt == 0 ){
+            System.out.println(cntTotal/2);
+        } else {
+            System.out.println("Too chaotic");
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
